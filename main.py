e23d989e49a83487d87849349830c6ef93fdfc4a
Sangkwun <wer2774@gmail.com>
2018-09-28 21:54:40 +0900

    download possible

--- main.py
+++ main.py
@@ -1,32 +1,37 @@
-import urllib3
+import os
+import time
+import urllib.request
 
 from selenium import webdriver
 from bs4 import BeautifulSoup
 from multiprocessing import Process, Queue
 from selenium.common.exceptions import NoSuchElementException
+from urllib import parse
 
 
 class Google_crawller(Process):
-    def __init__(self, term_queue, pdf_url_queue, os="window"):
+    def __init__(self, term_queue, pdf_url_queue, os):
 
         super(Google_crawller, self).__init__()  
         self._term_queue = term_queue
         self._pdf_url_queue = pdf_url_queue
 
-        self._driver_path = ""
         self._next = False
-        
-        if os == "windor":
-            self._driver_path = "./driver/chromedriver_window.exe"
+        self._open_chrome(os)
+        self._duration = 10
+
+        while self._driver is None:
+            time.sleep(3)
+
+    def _open_chrome(self, os):
+        if os == "window":
+            _driver_path = "./driver/chromedriver_window.exe"
         elif os == "mac":
-            self._driver_path = "./driver/chromedriver_mac"
+            _driver_path = "./driver/chromedriver_mac"
         else:
-            self._driver_path = "./driver/chromedriver_linux"
+            _driver_path = "./driver/chromedriver_linux"
 
-        self._driver = self._open_chrome()
-
-    def _open_chrome(self):
-        driver = webdriver.Chrome(self._driver_path)
+        driver = webdriver.Chrome(_driver_path)
         driver.implicitly_wait(3)
         driver.get('https://google.com')
         self._driver = driver
@@ -36,50 +41,92 @@ class Google_crawller(Process):
 
     def _search_term(self, search_term):
         self._driver.find_element_by_id('lst-ib').send_keys('{} filetype:pdf'.format(search_term))
-        self._driver.find_element_by_name('btnK').click()
+        time.sleep(self._duration)
+        self._driver.find_element_by_name('btnK').submit()
+        time.sleep(self._duration)
 
-    def _get_result(self, driver):
-        urls = []
+    def _get_result(self):
 
         html = self._driver.page_source
         soup = BeautifulSoup(html, 'html.parser')
 
         for g in soup.find_all(class_='g'):
-            url = g.cite.text
-            urls.append(url)
+            url = g.a['href']
+            self._pdf_url_queue.put(url)
 
-        return urls
-
-    def _next_page(self, driver):
+    def _chk_nex_page(self):
         try:
-            self._driver.find_element_by_class_name('csb ch').click()
-
+            self._driver.find_element_by_id('pnnext')
+            return True
         except NoSuchElementException:
-            self._next = False
+            return False
+
+    def _next_page(self):
+
+        self._driver.find_element_by_id('pnnext').click()
+        time.sleep(self._duration)
 
     def run(self):
-        while self._image_queue.empty() == False:
+        while not self._term_queue.empty():
             try:
-                self._next = True
                 self._return_home()
-                term = self._queue.get()
+                term = self._term_queue.get()
                 self._search_term(term)
+                _next = self._chk_nex_page()
 
-                while self._next:
+                while _next:
                 
-                    urls = self._get_result()
-                    [self._pdf_url_queue.put(url) for url in urls]
-
+                    self._get_result()
                     self._next_page()
+                    _next = self._chk_nex_page()
+                    time.sleep(1)
 
-            except err:
+            except Exception as err:
                 print(err)
+                raise
 
-#class Downloader(Process):
+
+class Downloader(Process):
+    def __init__(self, pdf_url_queue, directory):
+        super(Downloader, self).__init__()
+        self._pdf_url_queue = pdf_url_queue
+        self._directory = directory
+
+    def _validate_url(self, url):
+        if url.startswith("http://") or url.startswith("https://"):
+            return url
+            
+        return "http://{}".format(url)
+
+    def run(self):
+        while True:
+            url = self._pdf_url_queue.get()
+            last = url.split('/')[-1]
+            filename = parse.unquote(last.split('.')[0]) +'.pdf'
+            full_path = self._directory + '/' + filename
+            url = self._validate_url(url)
+            print(url, full_path)
+            
+            if not os.path.exists(full_path):
+                urllib.request.urlretrieve(url, full_path)
 
 
-def download(term, os="window"):
+def download(term_list, os="mac", directory="./pdf", num_crawller=1, num_downloader=1):
+    term_queue = Queue()
+    pdf_url_queue = Queue()
+    [term_queue.put(term) for term in term_list]
+
+    downloaders = [Downloader(pdf_url_queue=pdf_url_queue, directory=directory) for i in range(num_downloader)]
+    crawllers = [Google_crawller(term_queue=term_queue, pdf_url_queue=pdf_url_queue, os=os) for i in range(num_crawller)]
+
+    for crawller in crawllers:
+        crawller.start()
+
+    for downloader in downloaders:
+        downloader.start()
 
 
 if __name__ == '__main__':
-    pass
+    download(["딥러닝"])
+    while True:
+        pass
--- test.py
+++ test.py
@@ -2,19 +2,13 @@ import urllib
 from bs4 import BeautifulSoup
 import requests
 import webbrowser
+from urllib import parse
 
-text = '검색어 filetype:pdf'
-text = urllib.parse.quote_plus(text)
+url = "http://www.keei.re.kr/web_keei/d_results.nsf/0/30BD4E9B4EC031C649258205002A52F6/$file/171124_%EC%A0%9C9%EA%B8%B0%20%EC%B0%A8%EC%84%B8%EB%8C%80%EC%97%90%EB%84%88%EC%A7%80%EB%A6%AC%EB%8D%94%EA%B3%BC%EC%A0%95%2010%EC%A3%BC%EC%B0%A8%202%ED%8A%B9%EA%B0%95%20%EB%B0%9C%ED%91%9C%EC%9E%90%EB%A3%8C.pdf"
+_directory="./pdf"
+last = url.split('/')[-1]
+filename = parse.unquote(last.split('.')[0]) +'.pdf'
+full_path = _directory + '/' + filename
 
-url = 'https://google.com/search?q=' + text
-
-response = requests.get(url)
-
-#with open('output.html', 'wb') as f:
-#    f.write(response.content)
-#webbrowser.open('output.html')
-
-soup = BeautifulSoup(response.text, 'html.parser')
-for g in soup.find_all(class_='g'):
-    print(g.text)
-    print('-----')
+print(url, full_path)
+urllib.request.urlretrieve(url, full_path)
